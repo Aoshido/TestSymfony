f593266455f6b747286d767643260d164c8ab237
Aoshido <Aoshido@gmail.com>
2015-01-12 13:34:59 +0000

    Bundle not commited

---
+++ src/Aoshido/studyBundle/AoshidostudyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Aoshido\studyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AoshidostudyBundle extends Bundle
+{
+}
---
+++ src/Aoshido/studyBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Aoshido\studyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('AoshidostudyBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
