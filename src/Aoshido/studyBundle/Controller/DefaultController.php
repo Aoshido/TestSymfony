e9982881a54f0d4b13894b7c8d0a90142df4985b
Aoshido <Aoshido@gmail.com>
2015-01-13 01:17:42 -0300

    Configuraciones Varias
     - DB Funcionando!
     - Entities up

--- src/Aoshido/studyBundle/Controller/DefaultController.php
+++ src/Aoshido/studyBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace Aoshido\studyBundle\Controller;
 
+use Aoshido\studyBundle\Entity\Pregunta;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -11,9 +13,18 @@ class DefaultController extends Controller
         return $this->render('AoshidostudyBundle:Default:index.html.twig', array('name' => $name));
     }
     
-    public function homepageAction()
+    public function homepageAction(Request $request)
     {
-        return $this->render('AoshidostudyBundle:Default:homepage.html.twig');
+        $pregunta = new Pregunta();
+        
+        $form = $this->createFormBuilder($pregunta)
+                ->add('contenido','text')
+                ->add('vof','boolean')
+                ->getForm();
+        
+        return $this->render('AoshidostudyBundle:Default:homepage.html.twig',array(
+            'form' => $form->createView(),
+        ));
     }
     public function cargar_preguntas()
     {
