fb9b0adf5dc5a3aa1cce070b2183b1adb4a38122
Aoshido <Aoshido@gmail.com>
2015-01-13 05:11:15 +0000

    Agregando Twitter Bootstrap

--- src/Aoshido/studyBundle/Controller/DefaultController.php
+++ src/Aoshido/studyBundle/Controller/DefaultController.php
@@ -2,22 +2,50 @@
 
 namespace Aoshido\studyBundle\Controller;
 
+use Aoshido\studyBundle\Entity\Pregunta;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
+class DefaultController extends Controller {
+
+    public function indexAction($name) {
         return $this->render('AoshidostudyBundle:Default:index.html.twig', array('name' => $name));
     }
-    
-    public function homepageAction()
-    {
-        return $this->render('AoshidostudyBundle:Default:homepage.html.twig');
-    }
-    public function cargar_preguntas()
-    {
+
+    public function homepageAction(Request $request) {
         
-        return $preguntas;
+        
+        $preguntas = $this->getDoctrine()->getRepository('AoshidostudyBundle:Pregunta')
+                            ->findAll();
+        
+        $pregunta = new Pregunta();
+        $form = $this->createFormPregunta($pregunta);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // perform some action, such as saving the task to the database
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($pregunta);
+            $em->flush();
+        }
+
+        return $this->render('AoshidostudyBundle:Default:homepage.html.twig', array(
+                    'form' => $form->createView(),
+                    'preguntas' => $preguntas,
+        ));
     }
+    
+    private function createFormPregunta(Pregunta $pregunta){
+        
+        $form = $this->createFormBuilder($pregunta)
+                ->add('contenido', 'text')
+                ->add('vof', 'choice', array(
+                    'choices' => array( TRUE => 'Verdadero', FALSE => 'Falso'),
+                    'required' => true,
+                ))
+                ->add('save', 'submit', array('label' => 'Agregar Pregunta'))
+                ->getForm();
+        return ($form);
+    }
+
 }
