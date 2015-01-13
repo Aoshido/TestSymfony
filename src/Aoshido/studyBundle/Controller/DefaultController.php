756977369b6f48c7b8ecbebb31e4f86f1cded292
Aoshido <Aoshido@gmail.com>
2015-01-13 01:43:25 -0300

    Modificado form

--- src/Aoshido/studyBundle/Controller/DefaultController.php
+++ src/Aoshido/studyBundle/Controller/DefaultController.php
@@ -6,29 +6,46 @@ use Aoshido\studyBundle\Entity\Pregunta;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
+class DefaultController extends Controller {
+
+    public function indexAction($name) {
         return $this->render('AoshidostudyBundle:Default:index.html.twig', array('name' => $name));
     }
-    
-    public function homepageAction(Request $request)
-    {
+
+    public function homepageAction(Request $request) {
+        
+        $preguntas =  $this->getDoctrine()
+                                ->getRepository('AoshidostudyBundle:Pregunta')
+                                    ->findAll();
+        
         $pregunta = new Pregunta();
-        
+
         $form = $this->createFormBuilder($pregunta)
-                ->add('contenido','text')
-                ->add('vof','boolean')
+                ->add('contenido', 'text')
+                ->add('vof', 'choice', array(
+                    'choices' => array('t' => 'Verdadero', 'f' => 'Falso'),
+                    'required' => true,
+                ))
+                ->add('save', 'submit', array('label' => 'Agregar Pregunta'))
                 ->getForm();
-        
-        return $this->render('AoshidostudyBundle:Default:homepage.html.twig',array(
-            'form' => $form->createView(),
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // perform some action, such as saving the task to the database
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($pregunta);
+            $em->flush();
+        }
+
+        return $this->render('AoshidostudyBundle:Default:homepage.html.twig', array(
+                    'form' => $form->createView(),
+                    'preguntas' => $preguntas,
         ));
     }
-    public function cargar_preguntas()
-    {
-        
+
+    public function cargar_preguntas() {
+
         return $preguntas;
     }
+
 }
