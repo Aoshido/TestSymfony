1d3ed5153fb4ce8271da001a6e1d588d24887520
Aoshido <Aoshido@gmail.com>
2015-01-13 06:13:48 +0000

    Agregado Respuestas y datos en BD

--- src/Aoshido/studyBundle/Controller/DefaultController.php
+++ src/Aoshido/studyBundle/Controller/DefaultController.php
@@ -8,16 +8,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller {
 
-    public function indexAction($name) {
-        return $this->render('AoshidostudyBundle:Default:index.html.twig', array('name' => $name));
+    public function indexAction() {
+        return $this->render('AoshidostudyBundle:Default:index.html.twig');
     }
 
-    public function homepageAction(Request $request) {
-        $preguntas = $this->getDoctrine()->getRepository('AoshidostudyBundle:Pregunta')
-                            ->findAll();
-        
+    public function abmPreguntasAction(Request $request) {
+
+        $preguntas = $this->getDoctrine()
+                ->getRepository('AoshidostudyBundle:Pregunta')
+                ->findAll();
+
         $pregunta = new Pregunta();
-        $form = $this->createFormPregunta($pregunta);
+
+        $form = $this->createFormBuilder($pregunta)
+                ->add('contenido', 'text')
+                ->add('vof', 'choice', array(
+                    'choices' => array(TRUE => 'Verdadero', FALSE => 'Falso'),
+                    'required' => true,
+                ))
+                ->add('save', 'submit', array('label' => 'Agregar Pregunta'))
+                ->getForm();
         $form->handleRequest($request);
 
         if ($form->isValid()) {
@@ -27,23 +37,40 @@ class DefaultController extends Controller {
             $em->flush();
         }
 
-        return $this->render('AoshidostudyBundle:Default:homepage.html.twig', array(
+        return $this->render('AoshidostudyBundle:Default:abmPreguntas.html.twig', array(
                     'form' => $form->createView(),
                     'preguntas' => $preguntas,
         ));
     }
-    
-    private function createFormPregunta(Pregunta $pregunta){
-        
+
+    public function quizPreguntasAction(Request $request) {
+
+        $id = rand(1, 2);
+
+        $pregunta = $this->getDoctrine()
+                ->getRepository('AoshidostudyBundle:Pregunta')
+                ->find($id);
+
         $form = $this->createFormBuilder($pregunta)
-                ->add('contenido', 'text')
                 ->add('vof', 'choice', array(
-                    'choices' => array( TRUE => 'Verdadero', FALSE => 'Falso'),
-                    'required' => true,
-                ))
-                ->add('save', 'submit', array('label' => 'Agregar Pregunta'))
+                    'choices' => array(TRUE => 'Verdadero', FALSE => 'Falso'),
+                    'required' => true,))
+                ->add('save', 'submit', array('label' => 'Responder!'))
                 ->getForm();
-        return ($form);
+        
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // perform some action, such as saving the task to the database
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($pregunta);
+            $em->flush();
+        }
+
+        return $this->render('AoshidostudyBundle:Default:quizPreguntas.html.twig', array(
+                    'form' => $form->createView(),
+                    'pregunta' => $pregunta,
+        ));
     }
 
 }
